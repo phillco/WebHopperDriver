acd6410ef140e48eff559617ac6de40517a87f8b
Phillip Cohen <pc@phillipcohen.net>
2011-11-02 12:28:13 -0500

    Started to clean up the parser code.

--- src/hopperdriver/Main.java
+++ src/hopperdriver/Main.java
@@ -1,12 +1,9 @@
 package hopperdriver;
 
-import info.growl.*;
-
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("HopperDriver v1 started!");
-
-
         ShallowFetcher.run();
     }
 }
---
+++ src/hopperdriver/RowParser.groovy
@@ -0,0 +1,51 @@
+package hopperdriver
+
+class RowParser {
+
+    public static Course parseRow(row) {
+        def course = new Course(name: row.td[8].text());
+        try {
+            course.detailsLinkId = row.td[8].div?.a?.@id;
+            course.open = (row.td[2].text() == 'Open');
+
+            // Calculate the number of free/used seats.
+            def seatsSplit = row.td[3].toString().split('/');
+            if (seatsSplit.size() == 2) {
+                course.capacity = (seatsSplit[1] as Integer);
+                course.seatsUsed = course.capacity - (seatsSplit[0] as Integer);
+            }
+
+            course.instructorConsentRequired = (row.td[4] == 'Y');
+            course.reqCode = (row.td[5])
+
+            // Store the course's ZAP (our ID).
+            course.zap = row.td[6].toString().length() > 0 ? Integer.parseInt(row.td[6].toString().trim()) : 0;
+
+            // Find the department (or create a new one).
+            def departmentString = row.td[7].toString().split('\\*')[0];
+            course.department = departmentString
+            def courseNumber = row.td[7].toString().split('\\*')[1].toString();
+
+            // Some courses are labs
+            if (courseNumber.endsWith("L")) {
+                course.isLab = true;
+                course.courseNumber = (courseNumber[0..-2] as Integer);
+            }
+            else
+                course.courseNumber = (courseNumber as Integer);
+
+            course.section = (row.td[7].toString().split('\\*')[2] as Character);
+
+            // Process the professors (sometimes there are multiple ones).
+            course.professors = row.td[9].div.input.@value.toString().split('<BR>')
+            course.room = row.td[10].toString().trim();
+            course.schedules = row.td[11].div.input.@value.toString().split('<BR>')
+            course.comments = row.td[12].toString();
+            course;
+        }
+        catch (Exception e) {
+            println "Error during course conversion of '${course}'...";
+            println e;
+        }
+    }
+}
