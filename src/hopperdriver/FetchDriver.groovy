e3cfd32e9a933129cb91503d9cdd9a00a07c0622
Phillip Cohen <pc@phillipcohen.net>
2011-11-02 22:08:14 -0500

    Renamed.

---
+++ src/hopperdriver/FetchDriver.groovy
@@ -0,0 +1,62 @@
+package hopperdriver
+
+import org.openqa.selenium.By
+
+import org.openqa.selenium.firefox.FirefoxDriver
+
+import java.util.concurrent.TimeUnit
+import org.openqa.selenium.remote.RemoteWebDriver
+
+import com.google.gson.Gson
+
+/**
+ * Handles the acquiring & extraction of data from WebHopper.
+ */
+class FetchDriver {
+
+    static def run() {
+
+        def courses = []
+
+        // Fetch all 20 pages.
+        for (int i = 1; i <= 20; i++) {
+            courses.addAll(fetchAndParsePage(i))
+            println "Page $i complete."
+        }
+
+        def json = new Gson().toJson(courses)
+        println "${courses.size()} courses parsed: $json"
+
+        // Save the json to a file.
+        new File('courses.json').write(json);
+    }
+
+    /**
+     * Creates a new WebDriver session, jumps to page <pageNumber> and extracts and returns the data.
+     */
+    static List<Course> fetchAndParsePage(int pageNumber) {
+
+        // Set up the WebDriver.
+        println "Fetching page $pageNumber..."
+        RemoteWebDriver driver = new FirefoxDriver()
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://hopper.austincollege.edu/hlive/webhopper");
+        driver.findElement(By.linkText("Search for Courses")).click();
+        driver.findElement(By.name("SUBMIT2")).submit();
+
+        // Switch to the right page.
+        driver.findElement(By.name("JUMP*Grp:WSS.COURSE.SECTIONS*TOP")).sendKeys("$pageNumber");
+        driver.findElements(By.cssSelector(/input[value="JUMP"]/))[0].click()
+        Thread.sleep(500);
+
+        // Extract basic course data.
+        def html = Util.cleanAndConvertToXml(driver.pageSource)
+        List<Course> coursesOnPage = Util.findInNode(html) { it.name() == "table" && it.@summary == 'Sections' }.tbody.tr.collect { RowParser.parseRow(it) }.findAll { it != null }
+
+        // Fetch detailed information for each course (slow, requires a new page request for each).
+        coursesOnPage.each { PopupParser.getAndParseDetailsPage(it, driver); }
+
+        driver.close()
+        return coursesOnPage;
+    }
+}
